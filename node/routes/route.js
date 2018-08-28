78f8c43ac6303c61343bde8add576531896df864
rahulmukundan999 <31406444+rahulmukundan999@users.noreply.github.com>
2018-08-28 09:55:46 +0530

    Update route.js

--- node/routes/route.js
+++ node/routes/route.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Contact = require('./models/contacts');
 const Extension = require('./models/extension');
-
+const Extension1 = require('./models/extension');
+var builder = require('xmlbuilder');
+var fs = require('fs');
+var ext;
 //retrive contacts
 router.get('/contacts',(req,res,next)=>{
 Contact.find(function(err,contacts){
@@ -12,10 +15,59 @@ Contact.find(function(err,contacts){
 
 //Retrive extensions 
 router.get('/extensions',(req,res,next)=>{
+    var xml = builder.create('include');
+    
     Extension.find(function(err,extensions){
+
+      
+        for(var i=0; i<extensions.length; i++){
+            
+            var a='src/'+extensions[i].extensionno+'.xml';
+            
+       var b=i;
+            fs.exists(a,function (exists) {
+                
+               if(exists)
+                {
+                    
+                   
+               }
+               else
+               {
+                xml.ele('user',{'id':extensions[b].extensionno})
+                .ele('params') .ele('param',{'name':'password','value':extensions[b].extensionno}).up().ele('param',{'name':'vm-password','value':extensions[b].extensionno}).up()
+                .up()
+                .ele('variables')
+                .ele('variable',{'name':'toll_allow','value':'domestic,international,local'}).up()
+                .ele('variable',{'name':'accountcode','value':extensions[b].extensionno}).up()
+                .ele('variable',{'name':'user_context','value':'default'}).up()
+                .ele('variable',{'name':'effective_caller_id_name','value':'Extension '+extensions[b].extensionno}).up()
+                .ele('variable',{'name':'effective_caller_id_number','value':extensions[b].extensionno}).up()
+                .ele('variable',{'name':'outbound_caller_id_name','value':'$${'+extensions[b].displayname+'}'}).up()
+                .ele('variable',{'name':'outbound_caller_id_number','value':'$${'+extensions[b].outboundcid+'}'}).up()
+                .ele('variable',{'name':'callgroup','value':'techsupport'}).up()
+                .end({ pretty: true});
+             
+             
+                fs.writeFile(a,xml,function(err){
+
+
+
+
+                });
+                
+                  
+               }
+           });
+
+      
+       
+    }
+      
         res.json(extensions);
-    }) 
     });
+    
+});
     
 
 
@@ -46,6 +98,7 @@ router.post('/contact',(req,res,next)=>{
         }
         else{
             res.json({msg:'added succcessfully'});;
+            
         }
     })
 });
@@ -67,7 +120,9 @@ router.post('/extension',(req,res,next)=>{
             res.json({msg:'Failed to add extension'});
         }
         else{
-            res.json({msg:'added succcessfully'});;
+            
+           res.json({msg:'added'});
+
         }
     })
 });
@@ -75,7 +130,12 @@ router.post('/extension',(req,res,next)=>{
 
 
 //delete extension
-router.delete('/extension/:id',(req,res,next)=>{
+router.delete('/extension/:id/:extensionno',(req,res,next)=>{
+    ext=req.params.id;
+    name=req.params.extensionno;
+    fs.unlink(name+'.xml', function (err) {
+        
+      });
     Extension.remove({_id: req.params.id}, function(err, result){
         if(err)
         {
@@ -83,8 +143,10 @@ router.delete('/extension/:id',(req,res,next)=>{
         }
         else
         {
-            res.json(result);
-        }
+          
+        res.json(result);
+
+    }
     });
       
 });
