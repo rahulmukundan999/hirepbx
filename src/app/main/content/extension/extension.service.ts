4b607c4a397cd01bdea59edba23c733c8ed287f3
rahulmukundan999 <31406444+rahulmukundan999@users.noreply.github.com>
2018-08-28 18:13:28 +0530

    Update extension.service.ts

--- src/app/main/content/extension/extension.service.ts
+++ src/app/main/content/extension/extension.service.ts
@@ -14,14 +14,14 @@ export class ExtensionService {
 
 getExtensions()
 {
-  return this.http.get('http://127.0.0.1:3000/api/extensions').pipe (
+  return this.http.get('http://159.89.239.168:3000/api/extensions').pipe (
   map(res => res.json()));
 }
 addExtension(newExtension)
 {
   var headers = new Headers();
   headers.append('Content-Type','Application/Json');
-  return this.http.post('http://127.0.0.1:3000/api/extension',newExtension,{headers:headers}).pipe(
+  return this.http.post('http://159.89.239.168:3000/api/extension',newExtension,{headers:headers}).pipe(
   map(res => res.json()));
 }
 
