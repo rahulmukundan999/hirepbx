f18bee2fee77f276915c58b1e79c144ce1fcb812
rahulmukundan999 <31406444+rahulmukundan999@users.noreply.github.com>
2018-08-28 16:21:39 +0530

    Update route.js

--- node/routes/route.js
+++ node/routes/route.js
@@ -2,16 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Contact = require('./models/contacts');
 const Extension = require('./models/extension');
+const Trunk = require('./models/trunk');
 const Extension1 = require('./models/extension');
 var builder = require('xmlbuilder');
 var fs = require('fs');
-var ext;
+
+
+
+
+
+
+
+
 //retrive contacts
 router.get('/contacts',(req,res,next)=>{
 Contact.find(function(err,contacts){
     res.json(contacts);
 }) 
 });
+
+
+
+
 
 //Retrive extensions 
 router.get('/extensions',(req,res,next)=>{
@@ -22,13 +34,13 @@ router.get('/extensions',(req,res,next)=>{
       
         for(var i=0; i<extensions.length; i++){
             
-            var a='src/'+extensions[i].extensionno+'.xml';
+            var a='/usr/local/freeswitch/conf/directory/default/'+extensions[i].extensionno+'.xml';
             
        var b=i;
             fs.exists(a,function (exists) {
                 
                if(exists)
-                {
+                {   
                     
                    
                }
@@ -69,6 +81,55 @@ router.get('/extensions',(req,res,next)=>{
     
 });
     
+
+
+
+//retrive trunk
+router.get('/trunks',(req,res,next)=>{
+
+    
+
+
+    Trunk.find(function(err,trunks){
+        
+        var xml1 = builder.create('include');
+
+        for(var i=0; i<trunks.length; i++)
+        {
+            if(trunks[i].register==true)
+            {
+         xml1.ele('gateway',{'name':trunks[i].trunkname})
+        .ele('param',{'name':'proxy','value':trunks[i].trunkip}).up()
+        .ele('param',{'name':'register','value':trunks[i].register}).up()
+        .ele('param',{'name':'username','value':trunks[i].username1}).up()
+        .ele('param',{'name':'password','value':trunks[i].password}).up()
+        .up();
+            }
+    else
+    {
+        xml1.ele('gateway',{'name':trunks[i].trunkname})
+        .ele('param',{'name':'proxy','value':trunks[i].trunkip}).up()
+        .ele('param',{'name':'register','value':trunks[i].register}).up()
+        .ele('param',{'name':'caller-id-in-from','value':true}).up()
+        .up();
+
+    }
+}
+        xml1.end({ pretty: true});
+        fs.writeFile("/usr/local/freeswitch/conf/sip_profiles/internal.xml",xml1,function(err){
+
+         });
+        
+
+
+        res.json(trunks);
+    });
+
+    });
+    
+
+
+
 
 
 //add contacts
@@ -129,11 +190,38 @@ router.post('/extension',(req,res,next)=>{
 
 
 
+//add trunk
+router.post('/trunk',(req,res,next)=>{
+    var newTrunk = new Trunk({
+        trunkname: req.body.trunkname,
+        username1: req.body.username1,
+        password: req.body.password,
+        trunkip: req.body.trunkip,
+        register: req.body.register
+    })
+    
+
+    newTrunk.save((err,trunk)=>{
+        if(err)
+        {
+            res.json({msg:'Failed to add extension'});
+        }
+        else{
+            
+           res.json({msg:'added'});
+
+        }
+    })
+});
+
+
+
+
 //delete extension
 router.delete('/extension/:id/:extensionno',(req,res,next)=>{
     ext=req.params.id;
     name=req.params.extensionno;
-    fs.unlink(name+'.xml', function (err) {
+    fs.unlink('src/'+name+'.xml', function (err) {
         
       });
     Extension.remove({_id: req.params.id}, function(err, result){
@@ -168,6 +256,31 @@ Contact.remove({_id: req.params.id}, function(err, result){
 });
   
 });
+
+
+
+
+
+//delete trunk
+router.delete('/trunk/:id',(req,res,next)=>{
+    Trunk.remove({_id: req.params.id}, function(err, result){
+        if(err)
+        {
+            res.json(err);
+        }
+        else
+        {
+            res.json(result);
+        }
+    });
+      
+    });
+    
+
+
+
+
+
 
 router.put('/contact/:id',function(req,res){
 console.log('update a vdeo');
