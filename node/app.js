4da2c3c8b825495849610746cedfe8493e98dc00
dariniv <dariniv@outlook.com>
2018-08-30 16:33:45 +0530

    Update app.js

--- node/app.js
+++ node/app.js
@@ -32,6 +32,13 @@ app.use(bodyparser.json());
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.options("*",function(req,res,next){
+  res.header("Access-Control-Allow-Origin", req.get("Origin")||"*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   //other headers here
+    res.status(200).end();
+});
+
 app.use('/api',route);
 
 app.get('/',(req,res)=>{
