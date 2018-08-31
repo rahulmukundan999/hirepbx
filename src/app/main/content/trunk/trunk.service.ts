8205e8102c41fefdb6839a375cf39933832b6ccb
rahulmukundan999 <31406444+rahulmukundan999@users.noreply.github.com>
2018-08-31 11:38:59 +0530

    Update trunk.service.ts

--- src/app/main/content/trunk/trunk.service.ts
+++ src/app/main/content/trunk/trunk.service.ts
@@ -16,20 +16,20 @@ export class TrunkService {
 
 getTrunks()
 {
-  return this.http.get('http://localhost:3000/api/trunks').pipe (
+  return this.http.get('http://159.89.239.168:3000/api/trunks').pipe (
   map(res => res.json()));
 }
 addTrunk(newTrunk)
 {
   var headers = new Headers();
   headers.append('Content-Type','Application/Json');
-  return this.http.post('http://localhost:3000/api/trunk',newTrunk,{headers:headers}).pipe(
+  return this.http.post('http://159.89.239.168:3000/api/trunk',newTrunk,{headers:headers}).pipe(
   map(res => res.json()));
 }
 
 deleteTrunk(id)
 {
-  return this.http.delete('http://localhost:3000/api/trunk/'+id).pipe(map(res => res.json()));
+  return this.http.delete('http://159.89.239.168:3000/api/trunk/'+id).pipe(map(res => res.json()));
 }
 
 
